49b7fb2aea56b4025d2366adf496c895797766d1
Wednesday <yensonyu@gmail.com>
2020-12-06 00:28:05 -0800

    update markdown

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+const markdown = require('./utils/generateMarkdown')
 
 // array of questions for user
 const questions = [
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}`;
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#Licenses)
+  * [Contribution](#Contribution)
+  * [Test](#Test)
+  * [Question](#Questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## Questions
+  If you want to track the changes of the application, please feel free to visit my github at:
+  [https://github.com/${data.github}](https://github.com/${data.github})
+
+  In additions, please feel free to contact me if you have any questions about the application.
+  My email address is: <${data.email}>
+  `;
 }
 
 module.exports = generateMarkdown;
