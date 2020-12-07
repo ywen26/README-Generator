8467dea251bed30b53f34517d4088f49f15f3c6c
Wednesday <yensonyu@gmail.com>
2020-12-06 23:12:53 -0800

    update writeFile function

---
+++ READMESample.md
@@ -0,0 +1,36 @@
+
+  # Title
+
+  ## Description
+  This is my description
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#Licenses)
+  * [Contribution](#Contribution)
+  * [Test](#Test)
+  * [Question](#Questions)
+
+  ## Installation
+  This is installation instruction
+
+  ## Usage
+  This is usage information
+
+  ## License
+  MIT License
+
+  ## Contribution
+  This is contribution guidelines
+
+  ## Test
+  This is test instruction
+
+  ## Questions
+  If you want to track the changes of the application, please feel free to visit my github at:
+  [https://github.com/ywen26](https://github.com/ywen26)
+
+  In additions, please feel free to contact me if you have any questions about the application.
+  My email address is: <yensonyu@gmail.com>
+  
--- index.js
+++ index.js
@@ -1,65 +1,83 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-const markdown = require('./utils/generateMarkdown')
+// const util = require('util');
+const markdown = require('./utils/generateMarkdown');
+
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
     {
         type: 'input',
         name: 'title',
-        messeage: 'Enter your project title: ',
+        message: 'Enter your project title:',
     },
     {
         type: 'input',
         name: 'description',
-        messeage: 'Enter your project description: ',
+        message: 'Enter your project description:',
     },
     {
         type: 'input',
         name: 'installation',
-        messeage: 'Enter your project installation instruction: ',
+        message: 'Enter your project installation instruction:',
     },
     {
         type: 'input',
         name: 'usage',
-        messeage: 'Enter your project usage information: ',
+        message: 'Enter your project usage information:',
     },
     {
         type: 'input',
         name: 'contribution',
-        messeage: 'Enter your contribution guidelines: ',
+        message: 'Enter your contribution guidelines:',
     },
     {
         type: 'input',
         name: 'test',
-        messeage: 'Enter your project test instruction: ',
+        message: 'Enter your project test instruction:',
     },
     {
         type: 'list',
         name: 'license',
-        messeage: 'Please choose a license: ',
+        message: 'Please choose a license:',
         choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'None']
     },
     {
         type: 'input',
         name: 'github',
-        messeage: 'Enter your GitHub username: ',
+        message: 'Enter your GitHub username:',
     },
     {
         type: 'input',
         name: 'email',
-        messeage: 'Enter your Email address: ',
+        message: 'Enter your Email address:',
     },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('File created successfully!');
+    });
 }
 
 // function to initialize program
-function init() {
+async function init() {
+    console.log('Welcome');
+    try {
+        const answers = await inquirer.prompt(questions);
 
+        // const html = generateHTML(answers);
+        const readme = markdown(answers);
+
+        // await writeFileAsync('index.html', html);
+        writeToFile('READMESample.md', readme);
+
+    }   catch (err) {
+        console.log(err);
+    }
 }
 
 // function call to initialize program
