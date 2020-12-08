bec133cb5ef0dd6f7af9f0a0da31cc3adbe204c3
Wednesday <yensonyu@gmail.com>
2020-12-08 01:21:40 -0800

    update license part again

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,10 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  ![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
 
   # ${data.title}
+
+  ${badge(data.license)}
 
   ## Description
   ${data.description}
@@ -23,7 +24,8 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License
-  This application is covered under the license of ${data.license}.
+  Copyright (C) [${data.year}] [${data.name}]
+  ${licNotice(data.license)}
 
   ## Contribution
   ${data.contribution}
@@ -38,6 +40,86 @@ function generateMarkdown(data) {
   In additions, please feel free to contact me if you have any questions about the application.
   My email address is: <${data.email}>
   `;
+}
+
+function badge(answer) {
+    if (answers.license === 'Apache 2.0') {
+      answers.license = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    } else if (answers.license === 'MIT') {
+      answers.license = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    } else if (answers.license === 'GPLv3') {
+      answers.license = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    } else if (answers.license === 'Unlicense') {
+      answers.license = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+    } 
+}
+
+function licNotice(answer) {
+  if (answer === 'Apache 2.0') {
+    return `
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file 
+    except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the 
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+    either express or implied. See the License for the specific language governing permissions 
+    and limitations under the License.
+    `
+  } else if (answer === 'GPLv3') {
+    return `
+    This program is free software: you can redistribute it and/or modify it under the terms of the 
+    GNU General Public License as published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.  
+    If not, see <https://www.gnu.org/licenses/>.
+    `
+  } else if (answer === 'MIT') {
+    return `
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+    and associated documentation files (the "Software"), to deal in the Software without 
+    restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+    distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or 
+    substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+    BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    `
+  } else if (answer === 'Unlicense') {
+    return `
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, 
+    either in source code form or as a compiled binary, for any purpose, commercial or 
+    non-commercial, and by any means.
+
+    In jurisdictions that recognize copyright laws, the author or authors of this software 
+    dedicate any and all copyright interest in the software to the public domain. We make this 
+    dedication for the benefit of the public at large and to the detriment of our heirs and 
+    successors. We intend this dedication to be an overt act of relinquishment in perpetuity of 
+    all present and future rights to this software under copyright law.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+    BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    For more information, please refer to https://unlicense.org
+    `
+  }
 }
 
 module.exports = generateMarkdown;
